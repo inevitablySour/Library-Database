fe2d320c269724954307c196ee32cab0713a2fc2
Joel Kumi <joelkumi@protonmail.com>
2023-03-03 14:30:32 +0800

    Beta 3.0
    Added a favicon and corresponding controller method as well as update to the LibraryApplication.java

--- src/main/java/com/isb/library/LibraryApplication.java
+++ src/main/java/com/isb/library/LibraryApplication.java
@@ -2,12 +2,21 @@ package com.isb.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class LibraryApplication {
+@EnableWebMvc
+public class LibraryApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
 
 }
---
+++ src/main/java/com/isb/library/web/controller/FaviconController.java
@@ -0,0 +1,26 @@
+package com.isb.library.web.controller;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+public class FaviconController {
+
+    @RequestMapping("favicon.ico")
+    public void getFavicon(HttpServletResponse response) {
+        try {
+            InputStream is = getClass().getResourceAsStream("/static/favicon.ico");
+            response.setContentType("image/x-icon");
+            IOUtils.copy(is, response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
