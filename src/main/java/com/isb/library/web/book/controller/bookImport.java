740fda19d6235c96220eac24b1c8993cebf85b6c
Joel Kumi <joelkumi@protonmail.com>
2023-03-10 16:31:29 +0800

    Online Release

--- src/main/java/com/isb/library/web/book/controller/bookImport.java
+++ src/main/java/com/isb/library/web/book/controller/bookImport.java
@@ -179,7 +179,7 @@ public class bookImport {
      */
     public static void resetIncrement() throws SQLException {
 
-        Connection myConn = DriverManager.getConnection("jdbc:mariadb://ebc.isb.cn:3306/ebcisbcn_library?user=ebcisbcn_admin&password=AgentsRowsRodeoCaking80");
+        Connection myConn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/ebcisbcn_library?user=ebcisbcn_admin&password=AgentsRowsRodeoCaking80");
 
         Statement myStmt = myConn.createStatement();
         String sql = "ALTER TABLE books AUTO_INCREMENT = 1";
